6f9444a06df857acc143f34d6e78e6f7613261a3
EitanBorochov <86570074+EitanBorochov@users.noreply.github.com>
2025-07-18 02:12:32 +0300

    Tried fixing fonts

--- MauiApp2/MauiProgram.cs
+++ MauiApp2/MauiProgram.cs
@@ -11,8 +11,23 @@ public static class MauiProgram
             .UseMauiApp<App>()
             .ConfigureFonts(fonts =>
             {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                // Text fonts
+                fonts.AddFont("Text/Text-Black.ttf", "TextBlack");
+                fonts.AddFont("Text/Text-Bold.ttf", "TextBold");
+                fonts.AddFont("Text/Text-ExtraBold.ttf", "TextExtraBold");
+                fonts.AddFont("Text/Text-ExtraLight.ttf", "TextExtraLight");
+                fonts.AddFont("Text/Text-Italic.ttf", "TextItalic");
+                fonts.AddFont("Text/Text-Light.ttf", "TextLight");
+                fonts.AddFont("Text/Text-LightItalic.ttf", "TextLightItalic");
+                fonts.AddFont("Text/Text-Regular.ttf", "TextRegular");
+
+                // Title fonts
+                fonts.AddFont("Title/Title-Bold.ttf", "TitleBold");
+                fonts.AddFont("Title/Title-ExtraBold.ttf", "TitleExtraBold");
+                fonts.AddFont("Title/Title-Light.ttf", "TitleLight");
+                fonts.AddFont("Title/Title-Medium.ttf", "TitleMedium");
+                fonts.AddFont("Title/Title-Regular.ttf", "TitleRegular");
+                fonts.AddFont("Title/Title-SemiBold.ttf", "TitleSemiBold");
             });
 
 #if DEBUG
